1169eef563dadfd35a9dbabe4b95c1473e1a4f35
alex chalyy <alexchalyy@gmail.com>
2019-03-02 18:45:22 -0500

    buttons added!

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,16 +4,19 @@
 
 //  This array holds search strings for buttons that are used to search for gifs and output them on screen.
 
-var topics = ["husky", "alaskan malamute", "german shepard"];
+var topics = ["husky", "alaskan malamute", "german shepard", "collie", "pitbull", "sheltie", "bulldog", "rotvajler", "chihuahua", "poodle", "pug"];
 
-function PopulateButtons()  {
+function PopulateButtons() {
 
-//  This function clears button div and adds buttons dynamically to the div with text labels that are the same as topics array element values.
+    //  This function clears button div and adds buttons dynamically to the div with text labels that are the same as topics array element values.
 
+    //$("#buttons").remove(); //  This clears all previous buttons
 
-$("#buttons").append("<p>Test</p>");
-//$("#title").remove();
-
+    for (var c = 0; c < topics.length; c++) {   //  This populated the buttons div with topics array buttons
+        console.log(topics[c]);
+        //  This function populates the div with buttons
+        $("#buttons").append("<button type=\"button\" class=\"btn btn-primary m-1\" id = \"button-" + c.toString() + "\">" + topics[c] + "</button>");
+    }   
 }
 
 PopulateButtons();
