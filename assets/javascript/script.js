2f203627444b74b0d7b3352cf793c84ee1b679b4
alex chalyy <alexchalyy@gmail.com>
2019-03-02 17:43:37 -0500

    incorrect dynamic html

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+/*  This is the javascript that executes the game logic for GifTastic.
+
+    Written by Alex Chalyy on 3/2/2019. */
+
+//  This array holds search strings for buttons that are used to search for gifs and output them on screen.
+
+var topics = ["husky", "alaskan malamute", "german shepard"];
+
+function PopulateButtons()  {
+
+//  This function clears button div and adds buttons dynamically to the div with text labels that are the same as topics array element values.
+
+
+$("#buttons").append("<p>Test</p>");
+//$("#title").remove();
+
+}
+
+PopulateButtons();
