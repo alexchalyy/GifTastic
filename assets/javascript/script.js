e552db4221ba9f7ccf66e61f9c1943d418e1751e
alex chalyy <alexchalyy@gmail.com>
2019-03-03 19:32:58 -0500

    cannot read text box

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,10 +18,12 @@ function PopulateButtons() {
         console.log(topics[c]);
         //  This function populates the div with buttons
         $("#buttons").append("<button type=\"button\" class=\"btn btn-primary m-1\" id = \"button-" + c.toString() + "\">" + topics[c] + "</button>");
-    }   
+    }
 }
 
-function updatePage()   {
+//-----------------------------------------------------------------------------------------------------------
+
+function updatePage() {
 
     console.log("get call works!");
     console.log(response);
@@ -29,48 +31,42 @@ function updatePage()   {
 
 //----------------------------------------------------------------------------------------------------------
 
-
-
-//----------------------------------------------------------------------------------------------------------
-
-function PopulateGIFs(animal)   {
+function PopulateGIFs(animal) {
 
     //  This function populates the bootstrap card with 10 animal gif images of choice.
 
-    //below query connects to giphy API with key
-
-    //var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=wxu2jPOXlj0bnJJQl4bDrvxy4Og5iADT&limit=10");
-    
-    //xhr.done(function(data) { console.log("success got data", data); });
-
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=wxu2jPOXlj0bnJJQl4bDrvxy4Og5iADT&limit=10";
-
-    var q = "http://api.giphy.com/v1/gifs/feqkVgjJpYtjy?api_key=wxu2jPOXlj0bnJJQl4bDrvxy4Og5iADT&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=wxu2jPOXlj0bnJJQl4bDrvxy4Og5iADT&q=" + animal + "&limit=10&offset=0&rating=G&lang=en";
 
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
-        console.log(response.data[0].images.downsized.url);
+    }).then(function (response) {
         console.log(response);
-        $("#gifs").append("<img src=\"" + response.data[0].images.downsized.url.toString() + "\">");
-        console.log(response.data[0]);
-        console.log("husky");
-      });
+        $("#gifs").append("<p>husky<p><img src=\"" + response.data[0].images.downsized.url.toString() + "\">");
+    });
 }
 
 PopulateButtons();
 PopulateGIFs("husky");
 
-$("#sub").click(
-    
-    function Submit()   {
+$("#sub").on("click",
 
-    //  This function is called when the user clicks submit button.
-    //  It adds a new button to buttons if the text box is not empty, with textbox contents.
+    function (event) {
 
-    //topics.push($("s").text());
-    console.log(topics);
-    //$("#buttons").remove(); //  This clears all previous buttons
-    //PopulateButtons();
-});
+        //  This function is called when the user clicks submit button.
+        //  It adds a new button to buttons if the text box is not empty, with textbox contents.
+
+        event.preventDefault();
+        var a = $("s").val();
+        if (a == null) {
+            console.log("User did not enter any string. No search is performed.");
+            console.log(a);
+        }
+        else {
+            //console.log(topics);
+            topics.push($("s").val());
+            console.log(topics);
+            $("#buttons").remove(); //  This clears all previous buttons
+            PopulateButtons();
+        }
+    });
